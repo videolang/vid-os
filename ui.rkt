eb70f6c141d808c803ab58be4920fc1aefca40bc
Leif Andersen <leif@leifandersen.net>
2018-11-02 20:27:51 -0400

    Update UI to be (a bit) more functional.

--- ui.rkt
+++ ui.rkt
@@ -8,6 +8,9 @@
 (define screen-width 1920)
 (define screen-height 1080)
 
+(define recordings-folder (build-path (find-system-path 'home-dir) "recordings"))
+(make-directory* recordings-folder)
+
 (define view-width 960)
 (define view-height 540)
 
@@ -37,6 +40,25 @@
       (send n get-size-in-pixels)
       (send n get-hinting))))
 
+(define marks-file (build-path recordings-folder "marks.txt"))
+(define camera-render-settings
+  (make-render-settings #:destination (path->string (build-path recordings-folder
+                                                                (format "camera-~a"
+                                                                        (current-seconds))))
+                        #:format 'mp4
+                        #:split-av #t
+                        #:width 1920
+                        #:height 1080))
+
+(define presenter-render-settings
+  (make-render-settings #:destination (path->string (build-path recordings-folder
+                                                                (format "presenter-~a"
+                                                                        (current-seconds))))
+                        #:format 'mp4
+                        #:split-av #t
+                        #:width 1920
+                        #:height 1080))
+
 (define record-label
   (record-icon #:color "red"
                #:height 50))
@@ -57,6 +79,11 @@
     (new menu-item% [parent config-menu]
          [label "Saved Files"]
          [callback (λ (t e) (void))])
+    (new menu-item% [parent config-menu]
+         [label "Remove Marks"]
+         [callback (λ (t e)
+                     (delete-directory/files marks-file
+                                             #:must-exist #f))])
     (new menu-item% [parent config-menu]
          [label "Exit"]
          [callback
@@ -96,28 +123,25 @@
     (define control-row (new horizontal-pane% [parent this]))
     (define mark-button (new button% [parent control-row]
                              [label "MARK"]
-                             [font normal-font]))
+                             [font normal-font]
+                             [callback
+                              (λ (b e)
+                                (with-output-to-file marks-file
+                                  #:exists 'append
+                                  (λ ()
+                                    (displayln (current-seconds)))))]))
     (define rec-button (new button% [parent control-row]
                             [label record-label]
                             [font normal-font]))
     ;; Video player servers:
-
     (define camera-vps (new video-player-server%
                             [video (color "black")]
                             [canvas camera-screen]))
     (define presenter-vps (new video-player-server%
                                [video (color "black")]
                                [canvas presenter-screen]))
-    (send camera-vps record (make-render-settings #:destination (format "camera-~a" (current-seconds))
-                                                  #:format 'mp4
-                                                  #:split-av #t
-                                                  #:width 1920
-                                                  #:height 1080))
-    (send presenter-vps record (make-render-settings #:destination (format "presenter-~a" (current-seconds))
-                                                     #:format 'mp4
-                                                     #:split-av #t
-                                                     #:width 1920
-                                                     #:height 1080))
+    (send camera-vps record camera-render-settings)
+    (send presenter-vps record presenter-render-settings)
     (send camera-vps play)
     (send presenter-vps play)))
 
